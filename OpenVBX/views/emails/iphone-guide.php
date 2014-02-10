950d4a85b79f74c28cee267876ed719d32163138
fluffy <fluffy@beesbuzz.biz>
2014-02-09 18:08:32 -0800

    Fixing iPhone setup email

--- OpenVBX/views/emails/iphone-guide.php
+++ OpenVBX/views/emails/iphone-guide.php
@@ -1,4 +1,4 @@
-Follow the two steps below to get setup.  Remember to come back to this email when you're finished with each step.
+Follow the two steps below to get setup.  Remember to come back to this email when you're finished with each step.  Note: You'll have to open these links from your iOS device.
 
 1. Install the iPhone App:
 <?php echo site_url('iphone/install?email='.urlencode($email)); ?>
@@ -6,4 +6,4 @@ Follow the two steps below to get setup.  Remember to come back to this email wh
 2. Use the following URL for the quick setup.
 <?php echo site_url('iphone/install?email='.urlencode($email)); ?>
 
-Thats it!
+That's it!
